12f4059d91f36a160b74f05e838a631b5836dad3
R3DACTED <madmanpsalms@gmail.com>
2024-09-23 15:09:27 +1000

    AwareIcon Import Fix

--- com/planet_ink/coffee_mud/Abilities/Properties/Prop_AwareIcon.java
+++ com/planet_ink/coffee_mud/Abilities/Properties/Prop_AwareIcon.java
@@ -1,4 +1,21 @@
 package com.planet_ink.coffee_mud.Abilities.Properties;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.core.collections.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Libraries.interfaces.MaskingLibrary;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+import java.util.*;
 
 public class Prop_AwareIcon extends Property {
     @Override
@@ -18,5 +35,10 @@ public class Prop_AwareIcon extends Property {
 		super.setMiscText(newMiscText);
 	}
 
+    @Override
+    protected int canAffectCode()
+    {
+        return Ability.CAN_ROOMS;
+    }
 
 }
